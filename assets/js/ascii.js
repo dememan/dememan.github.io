1ff26292028bea9eaae77bfb6f8d6357a52b2817
Dememan <dememan@gmail.com>
2021-11-02 23:45:25 -0500

    validation cleanup

--- assets/js/ascii.js
+++ assets/js/ascii.js
@@ -1,62 +1,62 @@
-"use strict";
+"use strict"; {
+    var speedv = 250;
+    var anim;
 
-var speedv = 250;
-var anim;
+    function selectAnimation() {
+        var sAnim = document.getElementById("animations").value;
+        document.getElementById("txt").value = ANIMATIONS[sAnim];
 
-function selectAnimation() {
-    let sAnim = document.getElementById('animations').value;
-    document.getElementById('txt').value = ANIMATIONS[sAnim];
+    }
 
-}
+    function modifySize() {
+        var sSize = document.getElementById("sizes").value;
+        document.getElementById("txt").style.fontSize = sSize;
 
-function modifySize() {
-    let sSize = document.getElementById('sizes').value;
-    document.getElementById('txt').style.fontSize = sSize;
+    }
 
-}
+    function changeSpeed() {
+        if (document.getElementById("speed").value)
+            speedv = 50;
 
-function changeSpeed() {
-    if (document.getElementById('speed').value)
-        speedv = 50;
+        else
+            speedv = 250;
 
-    else
-        speedv = 250;
+    }
 
-}
+    function animateCaller() {
+        var textA = document.getElementById("txt").value;
+        document.getElementById("txt").value = "";
 
-function animateCaller() {
-    var textA = document.getElementById('txt').value;
-    document.getElementById('txt').value = "";
+        var lexi = textA.split("=====\n");
+        var counter = 0;
 
-    let lexi = textA.split("=====\n");
-    let counter = 0;
+        anim = setInterval((function () {
 
-    anim = setInterval((function () {
+            var nextVal = lexi[parseInt(counter % lexi.length)];
+            document.getElementById("txt").value = nextVal;
+            counter = counter + 1;
+        }), speedv);
 
-        let nextVal = lexi[parseInt(counter % lexi.length)];
-        document.getElementById('txt').value = nextVal;
-        counter = counter + 1;
-    }), speedv);
+    }
 
-}
-
-function animate() {
+    function animate() {
 
 
-}
+    }
 
-function start() {
-    document.getElementById('stop').disabled = false;
-    document.getElementById('start').disabled = true;
-    document.getElementById('animations').disabled = true;
-    animateCaller();
-}
+    function start() {
+        document.getElementById("stop").disabled = false;
+        document.getElementById("start").disabled = true;
+        document.getElementById("animations").disabled = true;
+        animateCaller();
+    }
 
-function stop() {
-    document.getElementById('start').disabled = false;
-    document.getElementById('stop').disabled = true;
+    function stop() {
+        document.getElementById("start").disabled = false;
+        document.getElementById("stop").disabled = true;
 
-    document.getElementById('animations').disabled = false;
-    clearInterval(anim);
-    selectAnimation();
+        document.getElementById("animations").disabled = false;
+        clearInterval(anim);
+        selectAnimation();
+    }
 }
