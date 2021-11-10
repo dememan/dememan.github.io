d3253f0c20836f1445008599e44fd34211fd472e
Dememan <dememan@gmail.com>
2021-11-09 22:39:48 -0600

    validations

--- JQPuzzle/fifteen.js
+++ JQPuzzle/fifteen.js
@@ -1,169 +1,104 @@
-"use strict";
-var empty_location = null;
-var started = false;
+$(function () {
 
-$(document).ready(function () {
-
+    const rows_columns = 4;
+    let Empty_Space_X = 3;
+    let Empty_Space_Y = 3;
     init();
-    $("#shufflebutton").click(shuffle);
+    $("#shufflebutton").on('click', shuffle);
+    //initialize and create the puzzle peices elements
+    function init() {
 
-});
+        //remove the available  divs under the puzzlearea
+        $("#puzzlearea div").remove();
 
-var init = function () {
+        var num = 1;
+        for (var i = 0; i < rows_columns; i++) {
+            for (var j = 0; j < rows_columns; j++) {
 
-    var divs = $("#puzzlearea div");
-
-    // initialize each piece        
-    divs.each(function (i) {
-        var div = divs[i];
-
-        // calculate x and y for this piece
-        var x = ((i % 4) * 100);
-        var y = (Math.floor(i / 4) * 100);
-
-        // set basic style and background
-        div.className = "puzzlepiece";
-        div.style.left = x + 'px';
-        div.style.top = y + 'px';
-        div.style.backgroundImage = 'url("background.jpg")';
-        div.style.backgroundPosition = -x + 'px ' + (-y) + 'px';
-
-        // store x and y for later
-        div.x = x;
-        div.y = y;
-
-        //set id
-        $(this).attr("id", "square_" + (x / 100 + 1) + "_" + (y / 100 + 1));
-
-    });
-
-    divs.each(function () {
-        //set class to movable piece of tile
-        if (is_movable(this)) {
-            $(this).addClass("movablepiece");
-        }
-
-        //set event handler to each tile
-        $(this).click(function () {
-            move_tile(this);
-        });
-    });
-
-};
-
-function move_tile(tile) {
-
-    if (is_movable(tile)) {
-
-        //relocate the tile
-        tile.style.left = empty_location.x + 'px';
-        tile.style.top = empty_location.y + 'px';
-        tile.style.backgroundPosition = -empty_location.x + 'px ' +
-            (-empty_location.y) + 'px';
-        tile.x = empty_location.x;
-        tile.y = empty_location.y;
-        $(tile).attr("id", "square_" + (tile.x / 100 + 1) + "_" +
-            (tile.y / 100 + 1));
-
-        //remove the class from previously movable tiles
-        $(".puzzlepiece").removeClass("movablepiece");
-
-        //add movablepiece class to the neighbours of the new empty tile
-        $(".puzzlepiece").each(function (i) {
-            if (is_movable(this)) {
-                $(this).addClass("movablepiece");
+                let content = num++;
+                var tile = document.createElement("div");
+                tile.classList.add("puzzlepiece");
+                tile.style.left = 100 * j + "px";
+                tile.style.top = 100 * i + "px";
+                tile.style.backgroundPosition = (0 - 100 * j) + "px" + " " + (0 - 100 * i) + "px";
+                tile.setAttribute("id", "peice_" + j + "_" + i);
+                tile.innerHTML = content;
+                tile.onmouseover = highlight;
+                tile.onmouseout = unhighlight;
+                tile.onclick = peiceClick;
+                if (i != 3 || j != 3) {
+                    document.getElementById("puzzlearea").appendChild(tile);
+                }
             }
-        });
-    }
-
-    if (started) {
-        //congratulate if the game is won
-        if (is_won()) {
-            alert("Congra");
         }
     }
-}
-
-function is_movable(tile) {
-    //is left neighbour empty tile?
-    if ((tile.x - 100) >= 0) {
-        if (!is_tile(tile.x - 100, tile.y)) {
-            empty_location = {
-                x: tile.x - 100,
-                y: tile.y
-            };
+    // Peice click event handler
+    function peiceClick() {
+        movePeice(this);
+    }
+    // on mouse event callback
+    function highlight() {
+        if (validMove(this)) {
+            this.classList.add("movablepiece");
+        }
+    }
+    // checking weather the next move is empty space 
+    function validMove(tile) {
+        var neighbors = getNeighbors();
+        if (neighbors.indexOf(tile.getAttribute("id")) != -1) {
             return true;
-        }
-    }
-    //is top neighbour empty tile?
-    if ((tile.y - 100) >= 0) {
-        if (!is_tile(tile.x, tile.y - 100)) {
-            empty_location = {
-                x: tile.x,
-                y: tile.y - 100
-            };
-            return true;
-        }
-    }
-    //is right neighbour  empty tile?
-    if ((tile.x + 100) <= 300) {
-        if (!is_tile(tile.x + 100, tile.y)) {
-            empty_location = {
-                x: tile.x + 100,
-                y: tile.y
-            };
-            return true;
-        }
-    }
-    //is bottom neighbour  empty tile?
-    if ((tile.y + 100) <= 300) {
-        if (!is_tile(tile.x, tile.y + 100)) {
-            empty_location = {
-                x: tile.x,
-                y: tile.y + 100
-            };
-            return true;
-        }
-    }
-    return false;
-}
-
-function is_tile(x, y) {
-    var div = $("#square_" + (x / 100 + 1) + "_" + (y / 100 + 1));
-    if (div.length == 0) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function shuffle() {
-    started = false;
-
-    //choose one of the movable pieces randomly and move it and repeat this 500 times
-    for (let j = 0; j < 500; j++) {
-        let movables = $(".puzzlepiece.movablepiece");
-        let i = parseInt(Math.random() * movables.length);
-        move_tile(movables[i]);
-    }
-
-    started = true;
-}
-
-function is_won() {
-    var isWon = true;
-
-    //check if each div is in its initial location before any shuffle was doen      
-    $(".puzzlepiece").each(function (i) {
-        var x = ((i % 4) * 100);
-        var y = (Math.floor(i / 4) * 100);
-        if ($(this).attr("id") != ("square_" + (x / 100 + 1) + "_" +
-                (y / 100 + 1))) {
-            isWon = false;
+        } else {
             return false;
         }
+    }
 
-    });
+    // on mouseleave event callback
+    function unhighlight() {
+        if (validMove(this)) {
+            this.classList.remove("movablepiece");
+        }
+    }
 
-    return isWon;
-}
+
+    // Swaps the selected peice
+    function movePeice(tile) {
+        var tempEX = Empty_Space_X;
+        var tempEY = Empty_Space_Y;
+        if (validMove(tile)) {
+            Empty_Space_X = parseInt(tile.style.left) / 100;
+            Empty_Space_Y = parseInt(tile.style.top) / 100;
+            tile.style.top = 100 * tempEY + "px";
+            tile.style.left = 100 * tempEX + "px";
+            tile.setAttribute("id", "peice_" + tempEX + "_" + tempEY);
+        }
+    }
+
+    // Shuffles the peices randomly  
+    function shuffle() {
+        for (var i = 0; i < 1000; i++) {
+            var neighbors = getNeighbors();
+            var rand = parseInt(Math.random() * neighbors.length);
+            var tile = document.getElementById(neighbors[rand]);
+            movePeice(tile);
+        }
+    }
+
+    // Checks peice around selected tile to see if they're empty 
+    function getNeighbors() {
+        var up = "peice_" + Empty_Space_X + "_" + (Empty_Space_Y - 1);
+        var down = "peice_" + Empty_Space_X + "_" + (Empty_Space_Y + 1);
+        var left = "peice_" + (Empty_Space_X - 1) + "_" + Empty_Space_Y;
+        var right = "peice_" + (Empty_Space_X + 1) + "_" + Empty_Space_Y;
+
+        var peice = [up, down, left, right];
+        var realpeice = [];
+
+        for (var i = 0; i < peice.length; i++) {
+            if (document.getElementById(peice[i]) != null) {
+                realpeice.push(peice[i]);
+            }
+        }
+        return realpeice;
+    }
+
+});
